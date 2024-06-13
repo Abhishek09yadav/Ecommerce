564c3b4e12e5d826589631264288bbf331722f89
Abhishek Yadav <monuy8830@gmail.com>
2024-06-13 15:33:43 +0530

    initial routes for pages setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
 
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+
+                    <Route path='/' element={<Shop/>}/>
+                    <Route path='/mens' element={<ShopCategory category='men'/>}/>
+                    <Route path='/kids' element={<ShopCategory category='kid'/>}/>
+                    <Route path='/women' element={<ShopCategory category='women'/>}/>
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
