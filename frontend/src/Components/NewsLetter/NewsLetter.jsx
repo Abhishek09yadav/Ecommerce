a774b19e290a8edf3e30b0316a9b4fb2c6d9a313
Abhishek Yadav <monuy8830@gmail.com>
2024-07-29 00:26:45 +0530

    modified Shop.jsx, NewCollections.jsx popular page and added more pages

---
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NewsLetter.css'
+import arrow_icon from "../Assets/arrow.png";
+
+const NewsLetter = () => {
+    return (
+        <div className={'NewsLetter'}>
+            <h1>Get Exclusive Offers On Your Email</h1>
+            <p>Subscribe to our newletter and stay updated < /p>
+            <div className={'email'}>
+                <input type={'email'} placeholder={'Email'}/>
+                <button type='submit' className="btn"> Subscribe{' '}
+                </button>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default NewsLetter;
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -4,15 +4,20 @@ import Item from "../Components/item/item";
 import Popular from "../Components/Popular/Popular";
 import Offers from "../Components/Offers/Offers";
 import NewCollections from "../Components/NewCollections/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Footer from "../Components/Footer/Footer";
+// import './Shop.css' // not needed for now
 
 const Shop = () => {
     return (
-        <>
+        <div className="shop-container">
             <Hero/>
             <Popular/>
             <Offers/>
             <NewCollections/>
-        </>
+            <NewsLetter/>
+            <Footer/>
+        </div>
     )
 }
 export default Shop;
