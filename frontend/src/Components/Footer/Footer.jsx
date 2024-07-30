02be6653d030610a6f8cc6df69f3cfdced188212
Abhishek Yadav <monuy8830@gmail.com>
2024-07-30 14:49:30 +0530

    added footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 import './Footer.css'
+import footer_logo from '../Assets/logo_big.png'
+
+import instagram_icon from '../Assets/instagram_icon.png'
+
+import pinterest_icon from '../Assets/pintester_icon.png'
+
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
 
 const Footer = () => {
     return (
         <div className="footer">
+            <div className={'footer-logo'}>
+                <img src={footer_logo}/>
+                <p>SHOPPER</p>
+            </div>
 
+
+            <div>
+                <ul className="footer-links">
+                    <li>Company</li>
+                    <li>Products</li>
+                    <li>Offices</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+
+            </div>
+            <div className="footer-social-icons">
+                <div className="footer-icons-containers">
+                    <img src={instagram_icon} alt="instagram"/>
+                </div>
+                <div className="footer-icons-containers">
+                    <img src={pinterest_icon} alt="instagram"/>
+                </div>
+                <div className="footer-icons-containers">
+                    <img src={whatsapp_icon} alt="instagram"/>
+                </div>
+
+            </div>
+            <div className="footer-copyright">
+                <hr/>
+                <p>Copyright @2024 - All Rights Reserved</p>
+            </div>
         </div>
+
     );
 };
 
