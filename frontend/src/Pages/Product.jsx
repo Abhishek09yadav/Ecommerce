c8f5716574339c26865fe89fbf293991d700f2c7
Abhishek Yadav <monuy8830@gmail.com>
2024-08-13 23:30:03 +0530

    Breadcrum.jsx for Product.jsx page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ShopContext} from "../Context/ShopContext";
+import {useParams} from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 function Product(props) {
+    const {all_products} = useContext(ShopContext);
+    const {productid} = useParams()
+    const product = all_products.find(e => e.id === Number(productid))
     return (
-        <div></div>
+
+        <div>
+            <Breadcrum product={product}/>
+        </div>
     );
 }
 
