dee109c0cb897cca7c70c4f9b7fee1757dc48123
Abhishek Yadav <monuy8830@gmail.com>
2024-08-16 00:28:53 +0530

    fixed Breadcrum.jsx not displaying arrow icon properly created ProductDisplay.jsx page for more details for a product - added image and rating in ProductDisplay.jsx for a product

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './Breadcrum.css'
 
-import {arrow_icon} from '../Assets/breadcrum_arrow.png';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
 
 const Breadcrum = (props) => {
     const {product} = props;
     return (
         <div className={'Breadcrum'}>
-            HOME <img src={arrow_icon} alt=""/> SHOP <img src={arrow_icon} alt=""/> {product.category} <img
+            HOME <img src={arrow_icon} alt=""/>
+
+            SHOP <img src={arrow_icon} alt=""/> {product.category} <img
             src={arrow_icon} alt=""/>{product.name}
         </div>
     );
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,15 +2,22 @@ import React, {useContext} from 'react';
 import {ShopContext} from "../Context/ShopContext";
 import {useParams} from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 function Product(props) {
-    const {all_products} = useContext(ShopContext);
-    const {productid} = useParams()
-    const product = all_products.find(e => e.id === Number(productid))
+    const {all_product} = useContext(ShopContext);
+    const {productId} = useParams()
+    console.log(productId);
+    console.log('context value is ', all_product[0])
+    console.log('all_products:', all_product);
+    const product = all_product.find((e) => e.id === Number(productId))
+
+
     return (
 
         <div>
             <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
         </div>
     );
 }
