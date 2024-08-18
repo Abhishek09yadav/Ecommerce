d3940327b6f6f760f62c5e7ecde67e28a0a1ab6a
Abhishek Yadav <monuy8830@gmail.com>
2024-08-18 22:39:13 +0530

    in Product.jsx added DescriptionBox.jsx for more product description

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,34 @@
+import './DescriptionBox.css'
+
+import React from 'react';
+
+const DescriptionBox = () => {
+    return (
+        <div className="DescriptionBox">
+            <div className="DescriptionBox-navigator">
+                <div className="DescriptionBox-nav-box">
+                    Description
+                </div>
+                <div className="DescriptionBox-nav-box-fade">
+                    Reviews (127)
+                </div>
+            </div>
+
+            <div className="DescriptionBox-Description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae eveniet unde! A doloribus
+                    iusto minima modi quo sed vero? Excepturi nihil obcaecati placeat, porro tempora totam veniam?
+                    Aliquam aperiam earum harum illum labore minima nesciunt obcaecati perferendis sunt ut.
+                    Laboriosam molestias nobis recusandae sapiente tempora tempore, temporibus vel voluptas.
+                </p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt excepturi incidunt odit
+                    quibusdam totam ut vitae voluptates? Facere laudantium, magni omnis quas quis quisquam
+                    reiciendis vel vitae voluptas? Doloribus laborum magni nostrum quisquam tenetur ut. Accusamus
+                    architecto atque beatae ea eius et, maxime mollitia quia quo saepe sed vel
+                </p>
+
+            </div>
+        </div>
+    );
+};
+
+export default DescriptionBox;
