10aaf684882f35a81096d34e6ec085b9c1387d40
Abhishek Yadav <monuy8830@gmail.com>
2024-08-19 23:42:46 +0530

    in Product.jsx added RelatedProducts.jsx for recommending more products

---
+++ frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -0,0 +1,25 @@
+import './RelatedProducts.css'
+import React from 'react';
+import data_products from '../Assets/data'
+import data_product from "../Assets/data";
+import Item from "../item/item";
+
+const RelatedProducts = () => {
+    return (
+        <div className={'RelatedProducts'}>
+            <h1>Related Products</h1>
+            <hr/>
+            <div className="RelatedProducts-item">
+
+                {data_product.map((item, i) => {
+                    return <Item key={i} id={item.id} name={item.name}
+                                 image={item.image} new_price={item.new_price}
+                                 old_price={item.old_price}/>
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default RelatedProducts;
+
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import {useParams} from "react-router-dom";
 import Breadcrum from "../Components/Breadcrums/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 function Product(props) {
     const {all_product} = useContext(ShopContext);
@@ -12,14 +13,13 @@ function Product(props) {
     // console.log('context value is ', all_product[0])
     // console.log('all_products:', all_product);
     const product = all_product.find((e) => e.id === Number(productId))
-
-
     return (
 
-        <div>
+        <div className={'Product'}>
             <Breadcrum product={product}/>
             <ProductDisplay product={product}/>
             <DescriptionBox/>
+            <RelatedProducts/>
         </div>
     );
 }
