6662ed8f334f54d37ff013a608e31fb4e0ecd624
Abhishek09yadav <monuy8830@gmail.com>
2024-09-01 22:44:47 +0530

    added hamburger menu for 800px devices

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useRef, useState} from 'react';
 import "./Navbar.css"
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
@@ -6,23 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {Link} from "react-router-dom";
 import {ShopContext} from "../../Context/ShopContext";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars, faChevronRight, faSquareCaretRight} from '@fortawesome/free-solid-svg-icons';
 
 
 function Navbar(props) {
     const {getTotalCartItems} = useContext(ShopContext);
-
-
     const [menu, setmenu] = useState("shop");
-
+    const menuRef = useRef();
     const scrollDown = () => {
         window.scrollBy(0, 110);
+    }
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
     }
     return (<div className="navbar">
             <div className="nav-logo">
                 <img src={logo}/>
                 <p>SHOPPER</p>
             </div>
-            <ul className="nav-menu">
+            <FontAwesomeIcon onClick={dropdown_toggle} className={'nav-dropdown'} icon={faSquareCaretRight}/>
+
+            <ul ref={menuRef} className="nav-menu">
                 <li onClick={() => {
                     setmenu("shop")
                 }}><Link to={'/'}>Shop</Link> {menu === 'shop' ? <hr/> : <></>}</li>
