9afb5d877f949fd478e721fe6498a83537cde8fe
Abhishek09yadav <monuy8830@gmail.com>
2024-09-22 21:16:50 +0530

    added navbar in admin page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,15 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from "./Components/Navbar.jsx";
+import Admin from "./Pages/Admin/Admin.jsx";
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-
+            <Navbar/>
+            <Admin/>
         </>
     )
 }
---
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Sidebar.css'
+
+function Sidebar(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Sidebar;
