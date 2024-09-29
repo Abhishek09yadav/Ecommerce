a7e6cfd96a963d74b2551331bb03b4576c5bcb9b
Abhishek09yadav <monuy8830@gmail.com>
2024-09-29 22:20:09 +0530

    AddProduct.jsx is now fully responsive

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -8,7 +8,7 @@ function AddProduct(props) {
     const [productDetails, setProductDetails] = React.useState({
         name: '',
         image: '',
-        category: '',
+        category: 'women',
         new_price: '',
         old_price: '',
 
@@ -17,35 +17,62 @@ function AddProduct(props) {
         setImage(event.target.files[0])
     }
     const changeHandler = (e) => {
-        setProductDetails(...productDetails, [])
+        setProductDetails({...productDetails, [e.target.name]: e.target.value})
+    }
+    const Add_Product = async () => {
+        console.log('ProductDetails', productDetails);
+        let responseData;
+        let product = productDetails;
+        let formData = new FormData();
+        formData.append('product', image)
+        await fetch('http://localhost:4000/upload', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            },
+            body: formData,
+
+        }).then(res => res.json()).then((data) => {
+            responseData = data
+        }).catch(error => {
+            console.error('Error during file upload:', error);
+        });
+        
+        if (responseData.success) {
+            product.image = responseData.image_url;
+            console.log(product);
+        }
     }
 
     return (
         <div className="AddProduct">
             <div className="AddProduct-itemfield">
-                <p>
-                    Product Title
-                </p>
-                <input type='text' name='name' placeholder='Type Here'/>
+                <p>Product Title</p>
+                <input type='text' name='name' value={productDetails.name} onChange={changeHandler}
+                       placeholder='Type Here'/>
             </div>
             <div className="AddProduct-price">
                 <div className="AddProduct-itemfield">
                     <p>
                         Price
                     </p>
-                    <input type='text' name='old_price' placeholder='Type Here'/>
+                    <input type='text' name='old_price' value={productDetails.old_price} onChange={changeHandler}
+                           placeholder='Type Here'/>
                 </div>
                 <div className=" AddProduct-itemfield ">
                     <p>
                         Offer Price
                     </p>
-                    <input type='text' name='new_price' placeholder='Type Here'/>
+                    <input type='text' name='new_price' value={productDetails.new_price} onChange={changeHandler}
+                           placeholder='Type Here'/>
                 </div>
             </div>
 
             <div className="AddProduct-itemfield">
                 <p> Product Category</p>
-                <select name={'category'} className={'AddProduct-selector '}>
+                <select name={'category'} className={'AddProduct-selector '} value={productDetails.category}
+                        onChange={changeHandler}
+                >
                     <option value={'women'}>Women</option>
                     <option value={'men'}>Men</option>
                     <option value={'kid'}>Kid</option>
@@ -59,7 +86,9 @@ function AddProduct(props) {
                     <input onChange={imagehandeler} type='file' name={'image'} id='file-input' style={{display: 'none'}}
                            hidden={true}/>
                 </div>
-                <button type="button" className="AddProduct-btn btn btn-lg no-border no-focus-outline">ADD</button>
+                <button type="button" onClick={Add_Product}
+                        className="AddProduct-btn btn btn-lg no-border no-focus-outline">ADD
+                </button>
             </div>
         </div>
     );
