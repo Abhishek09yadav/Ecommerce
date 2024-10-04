6c1699462d2bffd3ac65e43e3a747e9988ddb6cd
Abhishek09yadav <monuy8830@gmail.com>
2024-10-04 12:18:33 +0530

    in backend -Created Schema for use model -Created endpoint for registering the user

--- backend/index.js
+++ backend/index.js
@@ -87,6 +87,39 @@ app.get('/allproducts', async (req, res) => {
     res.send(products);
     console.log("All products Fetched");
 })
+//Schema creating for use model
+const Users = mongoose.model('Users', {
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    cartData: {type: Number},
+    date: {type: Date, default: Date.now},
+
+});
+// Creating endpoint for registering the user
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({email: req.body.email});
+    if (check) {
+        return res.status(400).json({success: false, error: 'Email already exists'})
+    }
+    let cart = {}
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name: req.body.Username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+    await user.save();
+    const data = {
+        user: user.id,
+
+    }
+    const token = jwt.sign(data, '')
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log(
