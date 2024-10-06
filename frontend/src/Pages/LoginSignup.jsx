a987bca05e0ff4daa92be589623dd035f5ffbb7a
Abhishek09yadav <monuy8830@gmail.com>
2024-10-06 21:02:59 +0530

    in backend - now login/signup option will properly work based on  user choice - login/signup ui is now fully responsive - api integration is still left

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './CSS/LoginSignup.css'
 
+
 const LoginSignup = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: '',
+        email: '',
+    })
+    const Login = () => {
+        console.log('Logging in...');
+
+    };
+    const changeHandler = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const SignUp = () => {
+        console.log('Signing up...');
+
+    };
+    const [state, setState] = useState("Login")
     return (
         <div className="login-signup">
             <div className="login-signup-cointainer">
-                <h1>Sign Up</h1>
+                <h1>{state}</h1>
                 <div className="login-signup-fields">
-                    <input type={'text'} placeholder={'Your Name'}/>
+                    {state === 'SignUp' ?
+                        <input onChange={() => {
+                            changeHandler(e)
+                        }} name={'username'} value={''} type={'text'} placeholder={'Your Name'}/> : <></>}
                     <input type={'email'} placeholder={'Your Email'}/>
                     <input type={'password'} placeholder={'Your Password'}/>
                 </div>
-                <button className="btn btn-lg login-signup-button">Sign Up</button>
-                <p className={'login-signup-login'}>Already have an account <span>Login</span></p>
+                <button onClick={() => {
+                    state === 'Login' ? Login() : SignUp()
+                }} className="btn btn-lg login-signup-button">{state}</button>
+                {state === 'SignUp' ? <p className={'login-signup-login'}>Already have an account <span onClick={() => {
+                    setState('Login')
+                }}>Login</span></p> : ''}
+                {state === 'Login' ? <p className={'login-signup-login'}>Create an account <span onClick={() => {
+                    setState('SignUp')
+                }}>Sign Up</span></p> : ''}
                 <div className="login-signup-agree">
                     <input type={'checkbox'} name={''} id={''}/>
                     <span>{' '}By continuing, I agree to terms of use and privacy policy</span>
