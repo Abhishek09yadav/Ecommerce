c210460b2ffd1cdeac058d321bb0de0ace11d808
Abhishek09yadav <monuy8830@gmail.com>
2024-10-24 15:35:53 +0530

    configring deployment

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import {Link} from "react-router-dom";
 import {ShopContext} from "../../Context/ShopContext";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
